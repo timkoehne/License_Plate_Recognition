43e9f0b5daab7653ac32dfcff5b31c1fe0bb99ee
Tim Köhne <20153452+timkoehne@users.noreply.github.com>
2024-12-14 18:20:00 +0100

    car detection inference and evaluation

--- inference.py
+++ inference.py
@@ -1,11 +1,15 @@
 import glob
-import cv2
-from darknet_python import darknet, darknet_images
+from typing import Tuple
 
-cfg_file = "/home/tim/test/test.cfg"
-names_file = "/home/tim/test/test.names"
-weights_file = "/home/tim/test/backup/test_final.weights"
-validation_files = "/home/tim/test/test_valid.txt"
+import tqdm
+import cv2
+from darknet_python import darknet
+
+cfg_file = "/home/tim/vehicles/vehicles.cfg"
+names_file = "/home/tim/vehicles/vehicles.names"
+weights_file = "/home/tim/vehicles/backup/vehicles_best.weights"
+# weights_file = "/home/tim/vehicles/backup/vehicles_30000.weights"
+validation_files = "/home/tim/vehicles/vehicles_valid.txt"
 
 
 # https://stackoverflow.com/a/71708600
@@ -20,41 +24,110 @@ def wait_until_cv2_window_exit(window):
             break
     cv2.destroyAllWindows()
 
+def roughly_equal(val1, val2, image_size):
+    difference = abs(val1 - val2)
+    if difference <= 0.15 * image_size:
+        return True
+    else:
+        return False
+
+def close_by(highest_confidence_detection_bounding_box, correct_bounding_box):
+    close_enough = True
+    for i in range(4):
+        max_size = width if i % 2 == 0 else height
+        if not roughly_equal(highest_confidence_detection_bounding_box[i], correct_bounding_box[i], max_size):
+            close_enough = False
+            break
+    return close_enough
+
+def load_darknet_image(filename):
+    image_bgr = cv2.imread(filename)
+    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+    image_resized = cv2.resize(image_rgb, (width, height), interpolation=cv2.INTER_LINEAR)
+
+    darknet_image = darknet.make_image(width, height, 3)
+    darknet.copy_image_from_bytes(darknet_image, image_resized.tobytes())
+    return darknet_image
+
+def load_image_label():
+    txtfile = filename[:-4] + ".txt"
+    with open(txtfile, "r") as f:
+        correct_values = f.readline().strip().split(" ")
+        correct_bounding_box = (float(correct_values[1]) * width, float(correct_values[2]) * height, float(correct_values[3]) * width, float(correct_values[4]) * height)
+    # print(f"image contains: Class: {class_names[int(correct_values[0])]} - Bounding Box: {bounding_box}")
+    return int(correct_values[0]), correct_bounding_box
+
+def calculate_results():
+    print(f"There were {correct_predictions} correct predictions out of {len(validation_images)} images")
+    print(f"There were {no_prediction} images where no predictions was made")
+    precision = true_positives / (true_positives + false_positives)
+    recall = true_positives / (true_positives + false_negatives)
+    f1_score = 2*(precision * recall)/(precision + recall)
+    print(f"Recall: {recall}")
+    print(f"Precision: {precision}")
+    print(f"F1 Score: {f1_score}")
+
+def display_detection(detection, image_filename):
+    image_bgr = cv2.imread(image_filename)
+    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+    image_resized = cv2.resize(image_rgb, (width, height), interpolation=cv2.INTER_LINEAR)
+    
+    # display the results on the console
+    darknet.print_detections([detection], True)
+    # draw some boxes and labels over what was detected
+    image_with_boxes = darknet.draw_boxes([detection], image_resized, colours)
+    cv2.imshow("annotated image", cv2.cvtColor(image_with_boxes, cv2.COLOR_RGB2BGR))
+    wait_until_cv2_window_exit("annotated image")
 
 network = darknet.load_net_custom(cfg_file.encode("ascii"), weights_file.encode("ascii"), 0, 1)
 class_names = open(names_file).read().splitlines()
 
 colours = darknet.class_colors(class_names)
 
-prediction_threshold = 0.5
+prediction_threshold = 0.125
 width = darknet.network_width(network)
 height = darknet.network_height(network)
 
 with open(validation_files, "r") as file:
     validation_images = file.read().splitlines()
-for filename in validation_images[:5]:
-    print(filename)
+    
+correct_predictions = 0
+no_prediction = 0
+true_positives = 0
+false_positives = 0
+true_negatives = 0
+false_negatives = 0
+for filename in tqdm.tqdm(validation_images):
+    # print(filename)
+    
+    correct_class, correct_bounding_box = load_image_label()
 
-    # use OpenCV to load the image and swap OpenCV's usual BGR for the RGB that Darknet requires
-    image_bgr = cv2.imread(filename)
-    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
-    image_resized = cv2.resize(image_rgb, (width, height), interpolation=cv2.INTER_LINEAR)
-
-    # create a Darknet-specific image structure with the resized image
-    darknet_image = darknet.make_image(width, height, 3)
-    darknet.copy_image_from_bytes(darknet_image, image_resized.tobytes())
-
-    # this is where darknet is called to do the magic!
+    darknet_image = load_darknet_image(filename)
     detections = darknet.detect_image(network, class_names, darknet_image, thresh=prediction_threshold)
     darknet.free_image(darknet_image)
+    # print("detections: " + str(detections))
+    
+    if len(detections) == 0:
+        no_prediction += 1
+        continue
+    
+    highest_confidence_detection = max(detections, key=lambda x: float(x[1]))
+    highest_confidence_detection_bounding_box = highest_confidence_detection[2]
+    # print(f"highest_confidence_detection is {highest_confidence_detection}")
+    
+    if highest_confidence_detection[0] == class_names[int(correct_class)]:
+        if close_by(highest_confidence_detection_bounding_box, correct_bounding_box):
+            true_positives += 1
+            correct_predictions += 1
+        else:
+            false_negatives += 1
+            # display_detection(highest_confidence_detection, filename)
+    else:
+        if close_by(highest_confidence_detection_bounding_box, correct_bounding_box):
+            false_positives += 1
+        else: # 
+            true_negatives += 1
 
-    # display the results on the console
-    darknet.print_detections(detections, True)
-
-    # draw some boxes and labels over what was detected
-    image_with_boxes = darknet.draw_boxes(detections, image_resized, colours)
-
-    cv2.imshow("annotated image", cv2.cvtColor(image_with_boxes, cv2.COLOR_RGB2BGR))
-    wait_until_cv2_window_exit("annotated image")
+calculate_results()
 
 darknet.free_network_ptr(network)
